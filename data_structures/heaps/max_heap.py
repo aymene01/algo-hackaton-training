370509fee8a3cdb56d8f1d87bc4731e578d9fe53
aymene01 <aymene.bousbia@veepee.com>
2024-10-20 22:56:35 +0200

    feat: Add heap data structures (MaxHeap and MinHeap) and heap sort function

---
+++ data_structures/heaps/max_heap.py
@@ -0,0 +1,19 @@
+class MaxHeap:
+    def __init__(self) -> None:
+        self.heap = []
+    
+    def insert(self, value):
+        ...
+    
+    def extract_min(self):
+        ...
+    
+    def _bubble_up(self, idx):
+        ...
+    
+    def _bubble_down(self):
+        ...
+    
+    def heapify(self):
+        ...
+
--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -2,5 +2,3 @@ class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
-
-
