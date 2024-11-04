fbfa015e68f41e39feaa409939b2a7f267585ee8
aymene01 <aymene.bousbia@veepee.com>
2024-11-04 11:00:32 +0100

    bubble sort

---
+++ algorithms/sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(array):
+    is_sorted = False
+    count = 0
+
+    while not is_sorted:
+        is_sorted = True
+        for i in range(len(array) - 1 - count):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i] 
+                is_sorted = False
+        count += 1
+
+    return array
