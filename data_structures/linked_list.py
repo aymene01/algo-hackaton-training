d7c993795c6cf8fa3fbb01ed9a3eabd2630e3905
aymene01 <aymene.bousbia@veepee.com>
2024-11-29 12:06:46 +0100

    update linked_list.py

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -2,3 +2,24 @@ class LinkedList:
     def __init__(self, value):
         self.value = value
         self.next = None
+    
+
+def print_ll(head):
+    current = head
+
+    while current:
+        print(current.val)
+        current = current.next
+    
+
+def reverse_ll(head):
+    prev = None
+    current = head
+
+    while current:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+
+    return prev
